a7590efa33ea6963e18d5ade21ca26bb8f5c52a9
Marko Baricevic <markobaricevic3778@gmail.com>
2022-11-22 16:01:54 +0100

    increase pool to 250 workers

--- cosi/utils/kv/indexer.go
+++ cosi/utils/kv/indexer.go
@@ -39,7 +39,7 @@ func IndexTxs(ctx context.Context, consumer *pubsub.EventSink, path string, star
 		return err
 	}
 
-	pool := pond.New(500, 5000)
+	pool := pond.New(250, 2500)
 
 	//TODO see if this can be concurrent
 	for i := start; i < end; i++ {
