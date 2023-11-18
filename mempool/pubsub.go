a818b7186d0c9ec076676667dc17d7c8b866960d
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-11-17 17:11:05 -0800

    updates

--- mempool/pubsub.go
+++ mempool/pubsub.go
@@ -1,8 +1,11 @@
 package mempool
 
 const (
+	// CredsEnvVar defines the environment variable that must be present when a using
+	// Google Cloud PubSub client.
 	CredsEnvVar = "GOOGLE_APPLICATION_CREDENTIALS"
 
+	// PubSub message attribute keys
 	AttrKeyMsgType   = "message_type"
 	AttrKeyChainID   = "chain_id"
 	AttrKeyTxHash    = "tx_hash"
@@ -11,5 +14,6 @@ const (
 	AttrKeyMsgSigner = "msg_signer"
 	AttrKeyTxMsgType = "tx_msg_type"
 
+	// MsgTypeCheckTxMsg defines the attribute value for 'AttrKeyMsgType'
 	MsgTypeCheckTxMsg = "mempool_tx_msg"
 )
